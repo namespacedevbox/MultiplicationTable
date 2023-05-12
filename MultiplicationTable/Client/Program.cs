4dc63688816e27c86fac428262dad3b7a6f8ed3e
Alexey Leonov <namespacedevbox@gmail.com>
2023-05-12 08:40:31 +0300

    upd

--- MultiplicationTable/Client/Program.cs
+++ MultiplicationTable/Client/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
-using MultiplicationTable;
 
 namespace MultiplicationTable
 {
@@ -14,7 +12,6 @@ namespace MultiplicationTable
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
--- MultiplicationTable/Pages/Index.razor.cs
+++ MultiplicationTable/Pages/Index.razor.cs
@@ -8,13 +8,15 @@ namespace MultiplicationTable.Pages
         private List<Questions> questions;
         private bool showResults;
         private Questions currentQuestion;
-        public ElementReference MyProperty { get; set; }
+
+        private List<AnswerResult> AnswerResults { get; set; } = new List<AnswerResult>();
+
+        public ElementReference AnswerField { get; set; }
+
         protected override void OnInitialized()
         {
-
-
             questions = new List<Questions>();
-            for (int x = 2; x <= 10; x++)
+            for (int x = 2; x <= 2; x++)
             {
                 for (int y = 2; y <= 10; y++)
                 {
@@ -30,26 +32,32 @@ namespace MultiplicationTable.Pages
             currentQuestion = questions.FirstOrDefault(q => q.Answer == null);
             if (currentQuestion == null)
             {
-                showResults = true;
+                CalcResult();
             }
         }
+
         private async Task Next()
         {
             NextQuestion();
-            await MyProperty.FocusAsync();
-        }
-
-        private void Skip()
-        {
-            currentQuestion.Answer = 0;
-            NextQuestion();
+            await AnswerField.FocusAsync();
         }
 
         private void CalcResult()
         {
+            AnswerResults.Clear();
+            AnswerResults.Add(new AnswerResult
+            {
+                Title="Ok",
+                Count = questions.Where(x => x.IsCorrect == true).Count(),
+            });
+            AnswerResults.Add(new AnswerResult
+            {
+                Title="Wrong",
+                Count = questions.Where(x => x.IsCorrect == false).Count(),
+            });
             var ok = questions.Where(x => x.IsCorrect == true).ToList();
             var wrong = questions.Where(x => x.IsCorrect == false).ToList();
-
+            showResults = true;
         }
 
         public void Enter(KeyboardEventArgs e)
@@ -59,5 +67,24 @@ namespace MultiplicationTable.Pages
                 NextQuestion();
             }
         }
+
+        private string GetPointColor(AnswerResult order)
+        {
+            switch (order.Title)
+            {
+                case "Wrong":
+                    return "#e3001b";
+                case "Ok":
+                    return "#00783c";
+                default:
+                    return "";
+            }
+        }
+    }
+
+    public class AnswerResult
+    {
+        public string Title { get; set; }
+        public int Count { get; set; }
     }
 }
